9e7eb7e15691bef84b1f0d57a2d645f50ca40e09
gary909 <garywhite61@hotmail.com>
2022-12-06 16:15:03 +0100

    code working

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function dontGiveMeFive(start, end) {
-    return 0;
+    let myCnt = 0;
+    for (var i = start; i <= end; i++) {
+        if (String(i).includes('5')) {
+            myCnt = myCnt;
+        }
+        else {
+            myCnt = myCnt + 1;
+        }
+    }
+    return myCnt;
 }
 
 console.log(dontGiveMeFive(1, 9)); // 8
 console.log(dontGiveMeFive(4, 17)); // 12
+console.log(dontGiveMeFive(1, 22)); // 20
